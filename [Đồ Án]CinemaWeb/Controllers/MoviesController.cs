f9c584728e070bfe75162d71a8f18586915577db
Elys2105 <chaunguyen.22012005@gmail.com>
2024-10-29 02:06:43 +0700

    đẩy lên

---
+++ [Đồ Án]CinemaWeb/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _Đồ_Án_CinemaWeb.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult LichChieu()
+        {
+            return View();
+        }
+        public ActionResult LongAn()
+        {
+            return View();
+        }
+    }
+}
